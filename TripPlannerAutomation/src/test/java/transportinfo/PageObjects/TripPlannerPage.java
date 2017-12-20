37c99029bfc42d4083fe6d0935e2937fd901f413
Susan Walker <susan.walker@accesshq.com>
2017-12-20 11:52:25 +1100

    Completed Browser Driver factory methods
    New Package WebBrowserDrivers
    New BrowserDrivers class
    Updated Test_Steps to include more reliable access of WebElements given
    two browser compatibility expected.
    Updated TripPlannerPage to include new methods for Input Fields

--- TripPlannerAutomation/src/test/java/transportinfo/PageObjects/TripPlannerPage.java
+++ TripPlannerAutomation/src/test/java/transportinfo/PageObjects/TripPlannerPage.java
@@ -1,12 +1,12 @@
 package transportinfo.PageObjects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 
@@ -20,7 +20,7 @@ public class TripPlannerPage extends TripPlannerTemplate
 	@FindBy(how = How.ID, using = "search-input-To")
 	public WebElement txtbox_search_input_To;
 	
-	@FindBy(how = How.CLASS_NAME, using = "tp-search-go")
+	@FindBy(how = How.ID, using = "search-button")
 	public WebElement btn_Go;
 	
 	//Clear
@@ -37,16 +37,34 @@ public class TripPlannerPage extends TripPlannerTemplate
 	{
 		//locate the button
 		WebElement goButton;// = driver.findElement(By.id("search-button"));
-		
 		WebDriverWait wait=new WebDriverWait(driver, 20);
 		
-		goButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"search-button\"]")));
+		//goButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"search-button\"]")));
+		
+		goButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search-button")));
 		
 		//click "Go" button
-		goButton.click();
+		//goButton.click();
+		btn_Go.click();
 		Reporter.log("Clicked button \"Go\" to calculate Trip\n\r");
 		
 		//validate the button has been clicked
+		
+		
+	}
+
+	public void setSearchInputFromField(String fromLocation) 
+	{
+		txtbox_search_input_From.clear();
+		txtbox_search_input_From.sendKeys(fromLocation);
+		txtbox_search_input_From.sendKeys(Keys.TAB);
+		
+	}
+
+	public void setSearchInputToField(String toLocation) {
+		txtbox_search_input_To.clear();
+		txtbox_search_input_To.sendKeys(toLocation);
+		txtbox_search_input_To.sendKeys(Keys.TAB);
 		
 	}
 	
